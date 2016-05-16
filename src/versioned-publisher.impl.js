1564dcda3e7da6b262ec549087c74d03638197cc
Max Dumas <max.dumas@bodylabs.com>
2016-05-16 12:33:01 -0400

    Change noClobber back to force

--- src/versioned-publisher.impl.js
+++ src/versioned-publisher.impl.js
@@ -3,7 +3,7 @@ var semver = require('semver');
 var Publisher = require('urj').Publisher;
 var url = require('url');
 
-var publishWithVersion = function (version, source, baseUrl, noClobber, doneCallback) {
+var publishWithVersion = function (version, source, baseUrl, force, doneCallback) {
     var majorVersion = semver.major(version);
 
     var fullVersionUrl = url.resolve(baseUrl, version);
@@ -13,7 +13,7 @@ var publishWithVersion = function (version, source, baseUrl, noClobber, doneCall
     async.series([
             function (callback) {
                 console.log('Publishing new version to ' + fullVersionUrl + "...");
-                var publisher = new Publisher({ noClobber: noClobber });
+                var publisher = new Publisher({ noClobber: ! force });
                 publisher.publish(source, fullVersionUrl, callback);
             },
             function (callback) {
